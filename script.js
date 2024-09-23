e19c9d3488379bb5ccf761585d7da8994d422c70
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-09-23 08:43:00 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function printMessage(){
+    document.getElementById("print");
+    element.innerHTML = "Hello World";
+    element.style.display = "none";
+
+}
+
+function display(){
+    let element = document.getElementById("print")
+
+    element.style.display= "block"
+    element.style.color = "red"
+
+}
+
